0a0335c671c8cffb40266fe5a561c3e4ced429c6
htp0724 <70932219+htp0724@users.noreply.github.com>
2020-10-05 09:45:14 +0900

    Create Data.java

---
+++ private-information-security/Store_Performance_Test/model/Data.java
@@ -0,0 +1,15 @@
+package com.java.test.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Data {
+	
+	private String message;
+	private String date;
+	
+	private Data data;
+	
+}
