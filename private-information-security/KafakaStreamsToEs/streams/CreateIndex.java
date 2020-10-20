618d897b276db425f66644708dc34fd24e0f17d3
htp0724 <70932219+htp0724@users.noreply.github.com>
2020-10-20 16:08:09 +0900

    Update CreateIndex.java

--- private-information-security/KafakaStreamsToEs/streams/CreateIndex.java
+++ private-information-security/KafakaStreamsToEs/streams/CreateIndex.java
@@ -1,57 +1,49 @@
-package com.kafka.streams;
-
 import java.io.IOException;
 
-import org.apache.http.HttpHost;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 
+import com.kafka.streams.Authorization.Authorization;
 import com.kafka.streams.model.Index;
 
 public class CreateIndex {
 	
 	private static final Logger logger = LogManager.getLogger(CreateIndex.class);
+	private static String id = "user";
+	private static String password = "password";
+	private static String indexname = "performance_test";
 	
-	public void createindex() {
-		/*
-		 * 클러스터링 High level restclient 쿼리 RestHighLevelClient client = new
-		 * RestHighLevelClient( RestClient.builder( new HttpHost("localhost", 9300,
-		 * "http"), new HttpHost("localhost", 9300, "http"), new HttpHost("localhost",
-		 * 9300, "http"), new HttpHost("localhost", 9300, "http")));
-		 */
-
-		// index를 생성요청할 client 생성
-		RestHighLevelClient client = new RestHighLevelClient(
-				RestClient.builder(new HttpHost("172.17.104.17", 9200, "http")));
+	public static RestHighLevelClient createindex() {
 		
 		Index index = new Index();
-		String indexname = "testindex";
+		
+		RestHighLevelClient restClient = Authorization.highClientLogin(id, password);
 		
 		// index의 존재유무 확인
 		GetIndexRequest grequest = new GetIndexRequest(indexname);
 		boolean exists;
+		
 		try {
+			exists = restClient.indices().exists(grequest, RequestOptions.DEFAULT);;
 			
-			exists = client.indices().exists(grequest, RequestOptions.DEFAULT);;
 			// index 생성
 			if (exists == false) {
 				CreateIndexRequest request = new CreateIndexRequest(indexname);
 				request.mapping(index.indexmapping(), XContentType.JSON);
-				CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
+				CreateIndexResponse createIndexResponse = restClient.indices().create(request, RequestOptions.DEFAULT);
 				logger.info(createIndexResponse);
 			}
-			client.close();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		return restClient;
 	}
 }
